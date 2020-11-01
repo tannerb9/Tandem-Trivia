8358b2cbf61f13a426c483acb4c2c9a63f945306
tannerb9 <tanner-brainard@hotmail.com>
2020-11-01 00:15:44 -0500

    feat: created all serializers

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,4 +1,17 @@
 from rest_framework import serializers
 from .models import *
 
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'question_text']
 
+class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ['id', 'answer_text', 'true_or_false', 'question_id']
+
+class HighscoreSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Highscore
+        fields = ['id', 'initials', 'score']
