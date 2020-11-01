c16a04ac3a2be869de210c7be438023484b3c8b2
tannerb9 <tanner-brainard@hotmail.com>
2020-10-31 23:56:37 -0500

    feat: created Question, Answer, and Highscore models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.TextField()
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=200)
+    true_or_false = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Highscore(models.Model):
+    user_initials = models.CharField(max_length=3)
+    score = models.IntegerField()
--- backend/serializers.py
+++ backend/serializers.py
@@ -1,1 +1,4 @@
 from rest_framework import serializers
+from .models import *
+
+
